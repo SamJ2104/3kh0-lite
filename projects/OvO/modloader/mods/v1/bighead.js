99ba11eed3f3f9f566f59d0fde3550edd0bb2753
Sam <140335136+SamJ2104@users.noreply.github.com>
2024-06-15 11:55:58 +0100

    Update bighead.js

--- projects/OvO/modloader/mods/v1/bighead.js
+++ projects/OvO/modloader/mods/v1/bighead.js
@@ -8,32 +8,51 @@
         return runtime.running_layout.layers.find(x => x.name === "Layer 0");
     }
 
-    const getHead = () => {
-        return getLayer().instances.find(x => x.type.name === "t33");
-    }
-
     const isInLevel = () => {
-        return runtime.running_layout.sheetname == "Levels";
+        return runtime.running_layout.sheetname === "Levels";
     }
 
-    const bigheadMod = {
+    const bigLayerMod = {
         init() {
             this.scale = 2;
-            this.width = 32 * this.scale;
-            this.height = 32 * this.scale;
-            
+            this.enabled = false;
+
             runtime.tickMe(this);
-            globalThis.ovoBighead = this;
+            globalThis.ovoBigLayer = this;
+
+            window.addEventListener('keydown', (event) => {
+                if (event.key === 'Control') {
+                    this.enabled = !this.enabled;
+                }
+            });
         },
 
         tick() {
             if (isInLevel()) {
-                const head = getHead();
-                head.width = this.width;
-                head.height = this.height;
+                const layer = getLayer();
+                if (layer) {
+                    layer.instances.forEach(instance => {
+                        if (this.enabled) {
+                            instance.width = instance.originalWidth * this.scale;
+                            instance.height = instance.originalHeight * this.scale;
+                        } else {
+                            instance.width = instance.originalWidth;
+                            instance.height = instance.originalHeight;
+                        }
+                    });
+                }
             }
         }
     };
 
-    bigheadMod.init();
+    // Store original dimensions to reset correctly
+    const layer = getLayer();
+    if (layer) {
+        layer.instances.forEach(instance => {
+            instance.originalWidth = instance.width;
+            instance.originalHeight = instance.height;
+        });
+    }
+
+    bigLayerMod.init();
 })();
